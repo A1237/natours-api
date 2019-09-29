ecd0b97bf86ec99793cea6cf3617bb203e08dbae
Akash <akashorasad2000@gmail.com>
2019-09-29 15:55:31 +0530

    Refatored Api features using class

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,5 @@
 const Tour = require('../models/tourModel');
-
+const APIFeatures = require('../utils/apiFeatures')
 // exports.checkBody = (req, res, next) => {
 //   if (!req.body.name || !req.body.price) {
 //     return res.status(404).json({
@@ -9,56 +9,68 @@ const Tour = require('../models/tourModel');
 //   next();
 // };
 
+
+
+exports.aliasTopTours = (req, res, next) => {
+  req.query.sort = '-ratingsAverage'
+  req.query.limit = '5';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty'
+  next();
+}
+
 exports.getallTours = async (req, res) => {
   try {
-    console.log(req)
     console.log(req.query);
 
-    // const fields = req.query.fields.split(',').join(' ');
-    // console.log(fields);
-    // * Here on logging page it gives 2
-
     // 1A) Simple Filtering
-    const queryObj = {
-      ...req.query
-    };
-    const excludeFields = ['page', 'sort', 'limit', 'fields'];
-    excludeFields.forEach(el => delete queryObj[el]);
+    // const queryObj = {
+    //   ...req.query
+    // };
+    // const excludeFields = ['page', 'sort', 'limit', 'fields'];
+    // excludeFields.forEach(el => delete queryObj[el]);
 
-    // 1B) Advanced Filtering
-    // console.log(queryObj);
-    let queryStr = JSON.stringify(queryObj);
+    // // 1B) Advanced Filtering
+    // // console.log(queryObj);
+    // let queryStr = JSON.stringify(queryObj);
 
-    queryStr = queryStr.replace(/\b(gte|lte|lt|gt)\b/g, match => `$${match}`);
+    // queryStr = queryStr.replace(/\b(gte|lte|lt|gt)\b/g, match => `$${match}`);
 
-    // * method 1
-    let query = Tour.find(JSON.parse(queryStr));
+    // // * method 1
+    // let query = Tour.find(JSON.parse(queryStr));
 
-    // 2) SORTING
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy)
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    // * field limiting 
-    if (req.query.fields) {
-      const sortBy = req.query.fields.split(',').join(' ');
-      console.log(sortBy)
-      query = query.select(sortBy);
-    }
-
-
-    console.log(req.query, 'asmk');
-    //* 3) not completed FIELD LIMITING
-    // if (req.query.fields) {
-    //   query = query.select(fields);
+    // // 2) SORTING
+    // if (req.query.sort) {
+    //   const sortBy = req.query.sort.split(',').join(' ');
+    //   query = query.sort(sortBy);
+    // } else {
+    //   query = query.sort('-createdAt');
     // }
-    //* not completed FIELD LIMITING
+
+    // // * field limiting
+    // if (req.query.fields) {
+    //   const fields = req.query.fields.split(',').join(' ');
+    //   console.log(fields);
+    //   query = query.select(fields);
+    // } else {
+    //   query = query.select('-__v');
+    // }
+
+    // 4) PAGINATION
+
+    // const page = req.query.page * 1 || 1;
+    // const limit = req.query.limit * 1 || 100;
+    // const skip = (page - 1) * limit;
+
+    // query = query.skip(skip).limit(limit)
+
+    // if (req.query.page) {
+    //   const numTours = await Tour.countDocuments();
+    //   if (skip >= numTours) throw new Error('This page Does not exist');
+    // }
 
     // EXECUTE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query).filter().sort().limitFields().paginate();
+    const tours = await features.query;
 
     // * method 2
     // const query = await Tour.find().where('duration').equals(5).where('difficulty').equals('easy')
@@ -151,3 +163,16 @@ exports.deleteTour = async (req, res) => {
     });
   }
 };
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = Tour.aggregate([
+
+    ]);
+  } catch (error) {
+    res.status(400).json({
+      status: 'Fail',
+      message: error
+    });
+  }
+}
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -10,6 +10,10 @@ const router = express.Router();
 // router.param('id', tourController.checkID);
 
 router
+    .route('/top-5-cheap')
+    .get(tourController.aliasTopTours, tourController.getallTours)
+
+router
     .route('/')
     .get(tourController.getallTours)
     .post(tourController.createTour);
